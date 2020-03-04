d30431065c86822346472f8c4a94be644ed2d60c
leojiang <guangxiang.jiang@hongpucorp.com>
2020-03-04 13:46:15 +0800

    将pai_detectron 相关代码移出detectron

--- docker/pai_detectron2/train.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-pip install fire && python3 voc2coco.py voc2coco train_data/xml instances_train2017.json && python3 train_net.py --config-file /home/appuser/pai_detectron2/Base-RCNN-FPN.yaml OUTPUT_DIR /home/appuser/pai_detectron2/output/models | tee /home/appuser/pai_detectron2/output/logs/log.txt 
